31cc795b9dc8490f40495c7063da0c81da1ae614
Kristel Patti-Trujillo <kristel.trujillo@mymail.champlain.edu>
2023-10-25 13:52:05 -0400

    First powershell script

---
+++ week_8/first_powershell_script.ps1
@@ -0,0 +1,16 @@
+﻿# First powershell script
+
+# Get directory to export to
+cd C:\Users\champuser\CSI230_01\week_8
+$exportlocation = "C:\Users\champuser\CSI230_01\week_8\log.csv"
+
+# Clear screen
+clear
+
+# List available logs
+Get-EventLog -List
+
+# Prompt user to select log
+$readlog = Read-Host -Prompt "Please select a log"
+
+Get-EventLog -LogName $readlog -Newest 5 | Export-Csv -NoTypeInformation -Path $exportlocation
