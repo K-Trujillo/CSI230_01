7a8f24572b9525e3bad881f5b6f2a7743da326ab
Kristel Patti-Trujillo <kristel.trujillo@mymail.champlain.edu>
2023-11-01 14:59:53 -0400

    Line/Word Counter WIP

---
+++ week_10/line_word_counter.ps1
@@ -0,0 +1,5 @@
+﻿# Line/Word Counter
+# Script that looks at past assginments and finds the average, max, and min line and word counts
+# Script in SS uses Measure-Object to get "line" and "word" counts
+# Hint 1: Start by searching examples of cmdlet and how it is used
+# Hint 2: Make sure that you are running your scripts in your CSI230_01 folder
